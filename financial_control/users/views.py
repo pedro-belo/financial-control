6055b48385aceaf93aa178cace230e1815683450
Pedro Belo <pn999717@gmail.com>
2022-12-07 10:30:36 -0300

    update users app

--- financial_control/users/views.py
+++ financial_control/users/views.py
@@ -1,3 +1,4 @@
+from financial_control.users.models import User
 from django.db.transaction import atomic
 from financial_control.main import facade
 
@@ -11,8 +12,28 @@ from django.urls import reverse
 
 
 class LoginView(DjLoginView):
-    template_name = "login.html"
+    template_name = "users/login.html"
     form_class = AuthenticationForm
+
+    @property
+    def user_created(self):
+
+        user_creation = self.request.session.get("user_creation", {})
+        user = None
+
+        if user_creation is not None:
+
+            try:
+                user = User.objects.get(pk=user_creation["user_id"])
+                self.request.session["user_creation"] = {}
+
+            except User.DoesNotExist:
+                pass
+
+            except KeyError:
+                pass
+
+        return user
 
 
 class LogoutView(DjLogoutView):
@@ -21,12 +42,19 @@ class LogoutView(DjLogoutView):
 
 class UserCreationView(FormView):
     form_class = UserCreationForm
-    template_name = "register.html"
+    template_name = "users/register.html"
 
     @atomic
     def form_valid(self, form):
+
         user = form.save()
         facade.create_root_record(user=user, name="Histórico Financeiro")
+
+        self.request.session["user_creation"] = {
+            "confirm_account": False,
+            "user_id": user.id,
+        }
+
         return super().form_valid(form)
 
     def get_success_url(self) -> str:
